f9ece95ebb43521594ea39877e4c04ab5d20614c
VanHell108 <vanner108@gmail.com>
2025-07-28 17:46:47 +0300

    Fundamentals accordion test

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -1,6 +1,15 @@
 describe('fundamentals test', () => {
-  it('Contains correct header text', () => {
+  beforeEach(() => {
     cy.visit('/fundamentals')
+  })
+  it('Contains correct header text', () => {
     cy.get('[data-test="fundamentals-header"]').should('contain', 'Testing Fundamentals')
+  })
+  it('Accordion works correctly', () => {
+    cy.contains('Your tests will exist in a describe block.').should('not.be.visible')
+    cy.get('[data-test="accordion-item-1"] div[role="button"]').click()
+    cy.contains('Your tests will exist in a describe block.').should('be.visible')
+    cy.get('[data-test="accordion-item-1"] div[role="button"]').click()
+    cy.contains('Your tests will exist in a describe block.').should('not.be.visible')
   })
 })
